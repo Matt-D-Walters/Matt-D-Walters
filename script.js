99d0f3ed4ccb5b9ac63524da8d13333a676ea12c
Matt-D-Walters <waltemw@mail.uc.edu>
2023-12-05 19:51:59 -0500

    testing with new html, js, and css

--- script.js
+++ script.js
@@ -1,24 +1,18 @@
-// Wait for the DOM content to be fully loaded before executing the script
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', function () {
+  const links = document.querySelectorAll('nav a, .title-links a');
 
-    // Add smooth scrolling to anchor links in the navigation
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-      anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-  
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
+  links.forEach(link => {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      const targetId = this.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+
+      if (targetElement) {
+        targetElement.scrollIntoView({
           behavior: 'smooth'
         });
-      });
+      }
     });
-  
-    // Add an event listener to the "About Me" section to toggle more information
-    const aboutSection = document.getElementById('about');
-    aboutSection.addEventListener('click', function () {
-      // Toggle the class 'expanded' on the about section
-      this.classList.toggle('expanded');
-    });
-  
-  
   });
-  
+});
